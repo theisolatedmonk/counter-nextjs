e4225a22ca8519023c7297981b9e9adbfb7846ea
theisolatedmonk <96rahulseth@gmail.com>
2023-04-11 21:50:54 +0530

    design improved

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 export default function App (){
  const [count, setCount] = useState(0)
  const [theme, setTheam] = useState('')
- const [colour, setColour] = useState('')
+ const [colour, setColour] = useState('white')
  let color2  =''
 
 color2='red'
@@ -26,12 +26,13 @@ color2='red'
 
 
  return (
-  <main className="flex  items-center min-h-screen justify-center ">
-    <div className="gap-2 h-36 flex w-36">
-    <button onClick={decrementCount} className="h-10 w-14 bg-gray-500 rounded-md">-</button>
-    <p  className="h-10 w-12 bg-yellow-400 text-center p-2 rounded-md">{count}</p>
-    <p style={{background:colour}} className="h-10 w-12 border border-black text-center p-2 rounded-md">{theme}</p>
-    <button onClick={incrementCount} className="h-10 w-14 bg-gray-500 rounded-md">+</button>
+  <main className="flex flex-col gap-4 items-center min-h-screen justify-center ">
+    <div className="gap-2 h-10 flex bg-yellow-50 font-bold w-56 items-center justify-center rounded-xl shadow-orange-200 shadow-md"> COUNTER APP</div>
+    <div className="gap-2 h-40 flex bg-orange-100 w-56 items-center justify-center rounded-xl shadow-orange-200 shadow-md">
+    <button onClick={decrementCount} className="h-10 w-10 bg-gray-300  text-2xl font-bold rounded-full  shadow-gray-400 shadow-md">-</button>
+    <p  className="h-10 w-12 bg-white text-center p-2 rounded-md ">{count}</p>
+    <p style={{background:colour}} className="h-10 w-12 border  text-center p-2 rounded-md">{theme}</p>
+    <button onClick={incrementCount} className="h-10 w-10 bg-gray-300 text-2xl font-bold rounded-full shadow-gray-400 shadow-md">+</button>
   </div>
   </main>
 )
