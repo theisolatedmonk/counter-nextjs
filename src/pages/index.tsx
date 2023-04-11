53f66272c1402dd42178536ce51c4284d1ff76ce
theisolatedmonk <96rahulseth@gmail.com>
2023-04-11 21:31:28 +0530

    getstate function added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,26 @@
 import { useState } from "react";
 export default function App (){
- const [count, setCount] = useState(4)
+ const [count, setCount] = useState(0)
+ const [theme, setTheam] = useState('')
+ const [colour, setColour] = useState('')
+ let color2  =''
+
+color2='red'
+
 
  function decrementCount(){
   setCount(prevCount => prevCount - 1);
+  setTheam('blue')
+  setColour('blue')
+
 
  }
  function incrementCount(){
   setCount(prevCount => prevCount + 1);
+  setTheam('red')
+  setColour("red")
+  // setColour(style: {})
+
 
  }
 
@@ -15,9 +28,10 @@ export default function App (){
  return (
   <main className="flex  items-center min-h-screen justify-center ">
     <div className="gap-2 h-36 flex w-36">
-    <button onClick={decrementCount} className="h-10 w-10 bg-gray-500 rounded-md">-</button>
-    <p className="h-10 w-10 bg-yellow-400 text-center p-2 rounded-md">{count}</p>
-    <button onClick={incrementCount} className="h-10 w-10 bg-gray-500 rounded-md">+</button>
+    <button onClick={decrementCount} className="h-10 w-14 bg-gray-500 rounded-md">-</button>
+    <p  className="h-10 w-12 bg-yellow-400 text-center p-2 rounded-md">{count}</p>
+    <p style={{background:colour}} className="h-10 w-12 border border-black text-center p-2 rounded-md">{theme}</p>
+    <button onClick={incrementCount} className="h-10 w-14 bg-gray-500 rounded-md">+</button>
   </div>
   </main>
 )
